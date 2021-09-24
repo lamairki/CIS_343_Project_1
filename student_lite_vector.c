636f9e3478390aee118df8e4af3458886c49db54
Kirk LaMaire <lamairki@mail.gvsu.edu>
2021-09-24 10:45:04 -0400

    Bugs fixed, seg fault fixed, changed append from length - 1 to length, works now

--- student_lite_vector.c
+++ student_lite_vector.c
@@ -7,7 +7,7 @@ lite_vector* lv_new_vec(size_t type_size){
     lite_vector * this_vector = malloc(sizeof(lite_vector));
     this_vector -> length = 0;
     this_vector -> type_size = type_size;
-    this_vector -> data = malloc(10 * sizeof(type_size));
+    this_vector -> data = malloc(10 * sizeof(void*));
     this_vector -> max_capacity = 10;
     return this_vector;
 }
@@ -36,42 +36,38 @@ bool lv_clear(lite_vector* vec){
 }
 
 void* lv_get(lite_vector* vec, size_t index){
-    if (vec -> data[index] != NULL) {
+    if (vec != NULL) {
         return vec -> data[index];
     }
     return NULL;
 }
 
 static bool lv_resize(lite_vector* vec){
-    size_t vecLength = lv_get_length(vec);
-    void* tempData[vecLength];
+    void** tempData = malloc(2 * (vec -> length) * sizeof(void*));
+    if (tempData == NULL) {
+        return 0;
+    }
+    vec -> max_capacity *= 2;
     int i;
-    for (i = 0; i < vecLength; i++) {
-        tempData[i] = (vec -> data[i * (vec -> type_size)]);
+    for (i = 0; i < (vec -> length); i++) {
+        tempData[i] = vec -> data[i];
     }
     free(vec -> data);
-    vec -> data = malloc(2 * vecLength * sizeof(vec -> type_size));
-    vec -> data[0] = tempData[0];
-    vec -> max_capacity = 2 * vecLength;
+    vec -> data = tempData;
     return 1;
 
 }
 
 bool lv_append(lite_vector* vec, void* element){
-    if (sizeof(element) == vec -> type_size) {
-        if ((vec -> max_capacity - lv_get_length(vec)) > 1) {
-            vec -> data[vec -> length * vec -> type_size] = element;
-            vec -> length += 1;
-            return true;
-        }
-        else {
-            lv_resize(vec);
-            vec -> data[(vec -> length)] = element;
-            vec -> length = lv_get_length(vec);
-            return true;
-        }
+    if (element == NULL ) {
+        return 0;
     }
-    printf("OOPS\n");
-    return false;
+    if (vec -> length == vec -> max_capacity) {
+        lv_resize(vec);
+    }
+    vec -> data[vec -> length] = element;
+    vec ->length = vec -> length + 1;
+
+    return 1;
 
 }
